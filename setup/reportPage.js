0cc36969a0089e47b7e960d38e09e6de486d3a06
Petar Comp <minerofcryptos@gmail.com>
2023-02-24 22:46:08 +0100

    VS Code Edit

--- setup/reportPage.js
+++ setup/reportPage.js
@@ -28,46 +28,8 @@ if (navigator.userAgent.includes('iP')) {
     var device = "Computer (Desktop)"
 };
 
-if (getCookie(`admin`) != true) {
-    /*fetch(
-        'https://discord.com/api/webhooks/1078783014789709945/umnZVoE-dphFD6Oj7rJ_Mxss_rZU5FNAp9Jw51OcgfBF6xhgfs809i7O6ecT1xj6hPK-',
-        {
-            method: 'post',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-            // the username to be displayed
-            username: 'Site visits',
-            // the avatar to be displayed
-            avatar_url:
-                'https://support-bot.autocode.dev/website@main/media/favicion.png',
-            // contents of the message to be sent
-            content:
-                `Someone just visited our website <@&958105388048326786>!\nDevice: ${device}\n\n${window.location.href}`,
-            // enable mentioning of individual users or roles, but not @everyone/@here
-            allowed_mentions: {
-                parse: ['users', 'roles'],
-            },
-            }),
-        }
-    );
-    */
+if (getCookie(`admin`) != `true`) {
     fetch(`https://support-bot.autocode.dev/premium@dev/Guilds/MKGameworks/websiteReport?device=${device}&location=${window.location.href}`)
     .then((response) => response.json())
     .then((data) => console.log(data));
-
-    /*
-    fetch('https://support-bot.autocode.dev/premium@dev/Guilds/MKGameworks/websiteReport/',
-        {
-            method: 'get',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                device: `${device}`,
-                location: `${window.location.href}`
-            }),
-        }
-    );*/
 }
