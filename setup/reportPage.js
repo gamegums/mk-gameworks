56b69ec47e74950e9fff0150d692813c9c90dac8
Petar Comp <minerofcryptos@gmail.com>
2023-02-24 21:45:18 +0100

    VS Code Edit

--- setup/reportPage.js
+++ setup/reportPage.js
@@ -1,3 +1,25 @@
+// Cookie functions
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+// Device lookup
 if (navigator.userAgent.includes('iP')) {
     var device = "Apple phone/tablet"
 } else if (navigator.userAgent.includes('Android') || navigator.userAgent.includes('IEMobile') || navigator.userAgent.includes('Opera Mini')) {
@@ -6,26 +28,28 @@ if (navigator.userAgent.includes('iP')) {
     var device = "Computer (Desktop)"
 };
 
-fetch(
-'https://discord.com/api/webhooks/958287758852522034/UHkbjhXJFlpFpmgnUsdiUcQ7sK80eG3MRGhvnKt4Pgqr_BnT0I_NfghB1DUCdDpr6kjw',
-{
-    method: 'post',
-    headers: {
-    'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-    // the username to be displayed
-    username: 'Site visits',
-    // the avatar to be displayed
-    avatar_url:
-        'https://support-bot.autocode.dev/website@main/media/favicion.png',
-    // contents of the message to be sent
-    content:
-        `Someone just visited our website <@&958105388048326786>!\nDevice: ${device}\n\n${window.location.href}`,
-    // enable mentioning of individual users or roles, but not @everyone/@here
-    allowed_mentions: {
-        parse: ['users', 'roles'],
-    },
-    }),
+if (getCookie(`admin`) != `true`) {
+    fetch(
+        'https://discord.com/api/webhooks/958287758852522034/UHkbjhXJFlpFpmgnUsdiUcQ7sK80eG3MRGhvnKt4Pgqr_BnT0I_NfghB1DUCdDpr6kjw',
+        {
+            method: 'post',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            // the username to be displayed
+            username: 'Site visits',
+            // the avatar to be displayed
+            avatar_url:
+                'https://support-bot.autocode.dev/website@main/media/favicion.png',
+            // contents of the message to be sent
+            content:
+                `Someone just visited our website <@&958105388048326786>!\nDevice: ${device}\n\n${window.location.href}`,
+            // enable mentioning of individual users or roles, but not @everyone/@here
+            allowed_mentions: {
+                parse: ['users', 'roles'],
+            },
+            }),
+        }
+        );
 }
-);
